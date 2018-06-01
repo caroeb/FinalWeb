8142fd89f88fbcdcb34623e9d39e2655f13d0ce5
Carolina Escobar <carolinaescobar1997@hotmail.com>
2018-06-01 01:43:51 -0500

    taller dos implementado

--- index.js
+++ index.js
@@ -1,22 +1,98 @@
-const express = require ("express");
-const consolidate = require ("consolidate");
-const handlebars = require ("handlebars");
-engines = require('consolidate');
+const MongoClient = require("mongodb").MongoClient
+        express = require ("express"),
+        ObjectID = require('mongodb').ObjectID,
+        consolidate = require("consolidate"),
+        handlebars = require("handlebars");
+        engines = require('consolidate');
 
+var app = express(),
+db;
 
-const app = express();
+app.engine('hbs', consolidate.handlebars);
 
-app.engine("hbs", consolidate.handlebars);
-
-app.set("views", "./views");
-app.set("view engine", "hbs");
+app.set ('views', './views');
+app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-    res.render("index", {
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+    if (err) throw err;
+
+    db = client.db('fidget');
+
+    app.listen(3000);
+});
+
+app.get("/",(req, res) => {
+
+    var productos = db.collection('productos').find();
+
+    if (req.query.color) 
+        productos.filter({color : req.query.color});
         
+    if (req.query.precio){
+        console.log(req.query.precio);
+
+        productos.filter({precio : {
+            $gte: parseFloat(req.query.precio[0]),
+            $lte: parseFloat(req.query.precio[1])
+        }
+         });
+    }
+
+    if (req.query.lados) 
+         productos.filter({
+             lados: req.query.lados
+         });
+
+    productos.toArray((err, result) => {
+       
+        res.render("index", {
+            productos : result
+        })
+    });
+
+});
+
+app.get("/productos/:direccion", (req, res) => {
+    console.log("hola");
+    db.collection('productos').find (
+        {
+            direccion: req.params.direccion
+        })
+        .toArray((err, result) => {
+        console.log(result[0]);
+        res.render('producto', {
+            fidget: result[0]
+        });
     });
 });
 
-app.listen(3000, console.log ("conectado"));
+app.get('/producto/:id', (req, res) => {
+    db.collection('productos').find({ direccion: req.params.id }).toArray((err, result) => res.send(result))
+});
+
+app.get('/checkout', (req, res) => {
+    res.render('checkout');
+});
+
+app.get('/tienda', (req, res) => {
+    res.render('tienda');
+});
+
+app.get('/productosPorIds', (req, res) => {
+    console.log(req.query.ids);
+    var arreglo = req.query.ids.split(',');
+    arreglo = arreglo.map(function(id) {
+        return new ObjectID(id);
+    });
+    var prod = db.collection('productos')
+        .find({ _id: { $in: arreglo } })
+        .toArray((err, result) => {
+            res.send(result);
+        });
+});
+
+app.get("/error", (req, res) => {
+    res.render('error');
+});
